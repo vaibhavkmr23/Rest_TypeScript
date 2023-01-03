d1537b722e8aa220c3928606309f5af3b2cc7ffe
Vaibhav <vaibhav@loansride.com>
2023-01-03 11:55:06 +0530

    Finishing The rest Routes

--- routes/todos.ts
+++ routes/todos.ts
@@ -6,7 +6,7 @@ import { Router } from 'express';
 
 import { Todo } from '../models/todo';
 
-const todos: Todo[] = []; // specifying that todos is simply an array of Todo i.e. Array Type Syntax
+let todos: Todo[] = []; // specifying that todos is simply an array of Todo i.e. Array Type Syntax
 
 const router = Router();
 
@@ -20,6 +20,25 @@ router.post('/todo', (req, res, next) => {
         text: req.body.text,
     };
     todos.push(newTodo);
+    res.status(200).json({ message: 'Added Todo', todo: newTodo, todos: todos })
+})
+
+router.put('/todo/:todoId', (req, res, next) => {
+    const tId = req.params.todoId;
+    const todoIndex = todos.findIndex(todoItem => todoItem.id === tId);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text }
+        return res.status(200).json({
+            message: 'Update Todo',
+            todos: todos
+        })
+    }
+    res.status(401).json({ message: 'Could Not find Todo with this Id' })
+})
+
+router.delete('/todo/:todoId', (req, res, next) => {
+    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+    res.status(200).json({ message: 'Todo Deleted', todos: todos })
 })
 
 export default router;
